b6c10aef8934fb6748cc40dbb85c67b34de93808
Hassan-Zaref <168092892+Hassan-Zaref@users.noreply.github.com>
2024-05-16 19:42:47 +0300

    Add files via upload

---
+++ second milestone/GPS_READ.c
@@ -0,0 +1,33 @@
+#include "Variables &Functions Prototypes.c"
+#include "Distance_Manipulation.c"
+#include "dividing_Field_gps.c"
+#include "EEPROM.c"
+#include "GetDistance.c"
+#include "GPS_READ.c"
+#include "Initializations.c"
+#include "Input_output.c"
+#include "Send_Coodinates.c"
+#include "Systick_functions.c"
+#include "UART0 PC_2_TIVA.c"
+
+//reading the output of gps and putting it in gpsmoduleoutput array
+void gpsread()
+	{
+	do {//loop until logname = logname
+	flg=1;
+		for(z=0;z<7;z++){
+			recieved = read();
+	if(recieved!=gpslogname[z]){
+		flg=0;
+		break;
+	}
+	}
+}while(flg==0);
+memset(gpsmoduleoutput,0,lengtharrgps);
+fillcount=0;
+do{
+recieved = read();
+gpsmoduleoutput[fillcount++]=recieved;
+}while(recieved !=star);
+}
+	
